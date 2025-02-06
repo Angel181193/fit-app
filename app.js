60c008115bcafa6efb849294fb912e5592b64c08
Herraez Angel (VGRS - ES/Barcelona) <angel.herraez@vwgroupretail.es>
2025-02-06 18:48:20 +0100

    Actualización de la aplicación con nuevas funcionalidades

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const ejerciciosPorDia = {
-  lunes: [
-      { nombre: "Press inclinado con mancuernas", grupo: "Pecho", series: 4, realizadas: 0 },
-      { nombre: "Aperturas con mancuernas", grupo: "Pecho", series: 3, realizadas: 0 }
-  ],
-  martes: [
-      { nombre: "Remo con mancuerna", grupo: "Espalda", series: 4, realizadas: 0 },
-      { nombre: "Jalón al pecho en polea", grupo: "Espalda", series: 3, realizadas: 0 }
-  ]
-};
-
+    lunes: [
+        { nombre: "Press inclinado con mancuernas", grupo: "Pecho", series: 4, realizadas: 0 },
+        { nombre: "Aperturas con mancuernas", grupo: "Pecho", series: 3, realizadas: 0 }
+    ],
+    martes: [
+        { nombre: "Remo con mancuerna", grupo: "Espalda", series: 4, realizadas: 0 },
+        { nombre: "Jalón al pecho en polea", grupo: "Espalda", series: 3, realizadas: 0 }
+    ],
+    miércoles: [
+        { nombre: "Remo con mancuerna", grupo: "Espalda", series: 4, realizadas: 0 },
+        { nombre: "Jalón al pecho en polea", grupo: "Espalda", series: 8, realizadas: 0 }
+    ],
+    jueves: [
+        { nombre: "🔥 Correr", grupo: "HIT", series: 5, realizadas: 0 },
+        { nombre: "🔥 Remo en polea baja", grupo: "Espalda", series: 8, realizadas: 0 },
+        { nombre: "🔥 Remo en polea baja brazo", grupo: "Espalda", series: 5, realizadas: 0 },
+        { nombre: "🔥 Extensión de tríceps en polea", grupo: "Triceps", series: 8, realizadas: 0 },
+        { nombre: "🔥 Extensión de bíceps en polea", grupo: "Biceps", series: 8, realizadas: 0 },
+        { nombre: "🔥 Bíceps con mancuerna", grupo: "Biceps", series: 5, realizadas: 0 },
+        { nombre: "🔥 Press Francés", grupo: "Triceps", series: 5, realizadas: 0 },
+        { nombre: "🔥 Correr", grupo: "HIT", series: 10, realizadas: 0 }
+    ]
+  };  
 const selectDia = document.getElementById("select-dia");
 const listaEjercicios = document.getElementById("ejercicios-lista");
 const startWorkoutBtn = document.getElementById("start-workout");
