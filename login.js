9f9b17afb0fc4170e5434a894ff2a9d29553a2f5
Herraez Angel (VGRS - ES/Barcelona) <angel.herraez@vwgroupretail.es>
2025-02-13 18:06:42 +0100

    Corrigiendo el login y actualizando credenciales

--- login.js
+++ login.js
@@ -10,22 +10,23 @@ document.getElementById('login-form').addEventListener('submit', function(e) {
 
   // Realizamos la solicitud a Google Apps Script para obtener los usuariosff
 
-fetch("https://script.google.com/macros/s/AKfycbyF4mnCsMpXSwxFfMyHnjI6axYnPwlaCp6wa8QUOoLGJKUinwlVQzM0hRfkln2HVNhqBA/exec")
-  .then(response => response.json())
-  .then(users => {
-    console.log("Usuarios obtenidos:", users); // Depura los usuarios obtenidos
-    const validUser = users.find(user =>
-      user.Usuario.trim().toLowerCase() === usuario.toLowerCase() && user.Clave.trim() === clave
-    );
-    if (validUser) {
-      // Si encontramos al usuario válido
-      localStorage.setItem('usuarioLogueado', JSON.stringify(validUser));
-      window.location.href = "index.html";
-    } else {
-      document.getElementById('mensaje').textContent = "Credenciales incorrectas. Intenta de nuevo.";
+  fetch('https://script.google.com/macros/s/AKfycbwIHut9rfmxNIOgrDyprG3H1OwdiErOR7DR7WsNZuqoN09axngTM2MnwnzK1UvqXzGKaw/exec?user=Angel')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
-
-    })
+    return response.json();  // Intenta parsear la respuesta como JSON
+  })
+  .then(data => {
+    console.log(data);  // Aquí obtienes el JSON
+    if (data.status === 'success') {
+      // Haz algo con la respuesta exitosa
+      console.log('Usuario recibido:', data.message);
+    } else {
+      // Maneja el error en el frontend
+      console.log('Error:', data.message);
+    }
+  })
     .catch(error => {
       console.error("Error al obtener datos:", error);
       document.getElementById('mensaje').innerText = "Error al conectar con el servidor.";
